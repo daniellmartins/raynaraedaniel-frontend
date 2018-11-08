be4ffda120b333cf366b83b2af8c264bd20dbf6d
Daniel Martins <daniellmarttins@hotmail.com>
2018-11-08 17:35:38 -0200

    fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const next = require("next");
 
-const port = parseInt(process.env.APP_PORT, 10) || 3000;
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
