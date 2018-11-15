ebbd943712ef99d37ac6ac266d1862c47902fd71
Daniel Martins <daniellmarttins@hotmail.com>
2018-11-14 22:15:49 -0300

    fix import withApollo

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import React from "react";
 import App, { Container } from "next/app";
 import { ApolloProvider } from "react-apollo";
 
-import withApollo from "../lib/withApollo";
+import { withApollo } from "../lib";
 import { Page } from "../components";
 
 class MyApp extends App {
