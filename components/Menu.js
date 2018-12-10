ccf30fd84b8829ef8a21bfd495bd027f2c0a0023
Daniel Martins <daniellmarttins@hotmail.com>
2018-12-10 10:34:13 -0300

    working mobile menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,56 +1,111 @@
-import React from "react";
-import styled from "styled-components";
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
 
 import { theme } from "../config";
 import { MenuItem } from "./";
 import { Heart } from "./icons";
 
-export const Menu = () => (
-  <StyledMenu>
-    <MenuList>
-      <MenuItem scroll={false} href="/" label="Home" />
-      <li>
-        <Heart fill={theme.color.primary} width={14} height={10} />
-      </li>
-      <MenuItem
-        scroll={false}
-        href="/"
-        as="/nossa-historia"
-        label="Nossa História"
-      />
-      <li>
-        <Heart fill={theme.color.primary} width={14} height={10} />
-      </li>
-      <MenuItem
-        scroll={false}
-        href="/"
-        as="/nossa-galeria"
-        label="Nossa Galeria"
-      />
-      <li>
-        <Heart fill={theme.color.primary} width={14} height={10} />
-      </li>
-      <MenuItem
-        scroll={false}
-        href="/"
-        as="/padrinhos-e-madrinhas"
-        label="Padrinhos e Madrinhas"
-      />
-      <li>
-        <Heart fill={theme.color.primary} width={14} height={10} />
-      </li>
-      <MenuItem
-        prefetch
-        href="/products"
-        as="/lista-de-presentes"
-        label="Lista de Presentes"
-      />
-    </MenuList>
-  </StyledMenu>
-);
+export class Menu extends Component {
+  state = { open: false };
+
+  handleMenu = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  render() {
+    return (
+      <StyledMenu>
+        <StyledButton onClick={this.handleMenu}>
+          <span />
+          <span />
+        </StyledButton>
+        <MenuList open={this.state.open}>
+          <MenuItem scroll={false} href="/" label="Home" />
+          <li>
+            <Heart fill={theme.color.primary} width={14} height={10} />
+          </li>
+          <MenuItem
+            scroll={false}
+            href="/"
+            as="/nossa-historia"
+            label="Nossa História"
+          />
+          <li>
+            <Heart fill={theme.color.primary} width={14} height={10} />
+          </li>
+          <MenuItem
+            scroll={false}
+            href="/"
+            as="/nossa-galeria"
+            label="Nossa Galeria"
+          />
+          <li>
+            <Heart fill={theme.color.primary} width={14} height={10} />
+          </li>
+          <MenuItem
+            scroll={false}
+            href="/"
+            as="/padrinhos-e-madrinhas"
+            label="Padrinhos e Madrinhas"
+          />
+          <li>
+            <Heart fill={theme.color.primary} width={14} height={10} />
+          </li>
+          <MenuItem
+            prefetch
+            href="/products"
+            as="/lista-de-presentes"
+            label="Lista de Presentes"
+          />
+        </MenuList>
+      </StyledMenu>
+    );
+  }
+}
 
 const StyledMenu = styled.div`
   /* position: relative; */
+`;
+
+const StyledButton = styled.button`
+  position: relative;
+
+  /* padding: 0.5rem; */
+  width: 2.5rem;
+  height: 2.5rem;
+
+  border-radius: 50%;
+  background-color: transparent;
+  border: none;
+
+  &:hover,
+  &:active,
+  &:focus {
+    background-color: #f7f7f7;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    display: none;
+  }
+
+  span {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    margin-top: -0.075rem;
+    margin-left: -0.75rem;
+    width: 1.5rem;
+    height: 0.15rem;
+
+    background-color: ${({ theme }) => theme.color.primary};
+
+    transform: rotate(90deg) translateX(-3px);
+
+    &:last-child {
+      transform: rotate(0) translateY(3px);
+    }
+  }
 `;
 
 const MenuList = styled.ul`
@@ -61,20 +116,60 @@ const MenuList = styled.ul`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  width: 100%;
-
   list-style: none;
+  overflow: hidden;
+
   margin: 0;
-  padding: 0;
+  padding: 2rem 0 0;
+  width: 100%;
 
   background-color: #ffffff;
 
-  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
-    display: block;
-  }
+  opacity: 0;
+  visibility: hidden;
+  pointer-events: none;
+
+  transform: translateY(-0.5rem);
+
+  transition: opacity 0.25s ease, transform 0.25s ease,
+    visibility 0s linear 0.25s;
+
+  ${({ open }) =>
+    open &&
+    css`
+      opacity: 1;
+      visibility: visible;
+      pointer-events: auto;
+
+      transform: none;
+      transition: opacity 0.25s ease, transform 0.25s ease;
+    `}
 
   li {
     display: inline-block;
+
+    margin-bottom: 1rem;
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    position: relative;
+    display: block;
+    top: auto;
+    left: auto;
+
+    padding: 0;
+    width: auto;
+
+    opacity: 1;
+    visibility: visible;
+    pointer-events: auto;
+
+    background-color: transparent;
+
+    transform: none;
+
+    li {
+      margin: 0;
+    }
   }
 `;
