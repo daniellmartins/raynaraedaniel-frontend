a16e413faded9d58bb1cf4fc10d1b26f706347c2
Daniel Martins <daniellmarttins@hotmail.com>
2018-12-17 06:47:58 -0300

    fix

--- components/Confirmation.js
+++ components/Confirmation.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Link from "next/link";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 import { Row, Col } from "react-styled-flexboxgrid";
@@ -17,108 +18,117 @@ export class Confirmation extends Component {
   render() {
     return (
       <div id="confimation">
-        <Col xs={12} md={6}>
-          <StyledMap>
-            <iframe
-              width="100%"
-              height="400px"
-              frameBorder="0"
-              style={{ border: 0 }}
-              src="https://www.google.com/maps/embed/v1/place?q=%20Esperan%C3%A7a%20PB%20sitio%20lago%20dourado&key=AIzaSyD-rGGZGoqwYkBom9p5aIK2zzgWAhUVxio"
-              allowFullScreen
-            />
-          </StyledMap>
-        </Col>
-        <Col xs={12} md={6}>
-          <StyledForm>
-            <Title>Confirmação de presença</Title>
-            <SubTitle>
-              Faça parte da nossa história de amor, confirme sua presença.
-            </SubTitle>
-            <Mutation mutation={CREATE_CONFIRMATION_MUTATION}>
-              {(createConfirmation, { loading, data }) => {
-                if (data)
-                  return (
-                    <p>
-                      Obrigado {data.createConfirmation.name} por confirmar sua
-                      presença, nos encontramos no grande dia!
-                    </p>
-                  );
-                return (
-                  <form
-                    action=""
-                    onSubmit={e => {
-                      e.preventDefault();
-                      createConfirmation({
-                        variables: {
-                          ...this.state,
-                          adults: parseInt(this.state.adults, 10)
-                        }
-                      });
-                    }}
-                  >
-                    <div className="form-group">
-                      <input
-                        required
-                        type="text"
-                        placeholder="Nome"
-                        name="name"
-                        value={this.state.name}
-                        onChange={this.handleInput}
-                      />
-                    </div>
-                    <div className="form-group">
-                      Você irá ao evento?{" "}
-                      <input
-                        name="confirmation"
-                        type="checkbox"
-                        id="yes"
-                        checked={
-                          this.state.confirmation === "yes" ? true : false
-                        }
-                        value="yes"
-                        onChange={this.handleInput}
-                      />{" "}
-                      <label htmlFor="yes">Sim</label>
-                      <input
-                        name="confirmation"
-                        type="checkbox"
-                        id="no"
-                        checked={
-                          this.state.confirmation === "no" ? true : false
-                        }
-                        value="no"
-                        onChange={this.handleInput}
-                      />{" "}
-                      <label htmlFor="no">Não</label>
-                    </div>
-                    <div className="form-group">
-                      Quantos adultos?{" "}
-                      <select
-                        required
-                        value={this.state.adults}
-                        name="adults"
-                        onChange={this.handleInput}
+        <Col xs={12}>
+          <Row>
+            <Col xs={12} md={6}>
+              <StyledMap>
+                <iframe
+                  width="100%"
+                  height="400px"
+                  frameBorder="0"
+                  style={{ border: 0 }}
+                  src="https://www.google.com/maps/embed/v1/place?q=%20Esperan%C3%A7a%20PB%20sitio%20lago%20dourado&key=AIzaSyD-rGGZGoqwYkBom9p5aIK2zzgWAhUVxio"
+                  allowFullScreen
+                />
+              </StyledMap>
+            </Col>
+            <Col xs={12} md={6}>
+              <StyledForm>
+                <Title>Confirmar Presença</Title>
+                <SubTitle>
+                  Faça parte da nossa história de amor, confirme sua presença.
+                </SubTitle>
+                <Mutation mutation={CREATE_CONFIRMATION_MUTATION}>
+                  {(createConfirmation, { loading, data }) => {
+                    if (data)
+                      return (
+                        <p>
+                          Obrigado {data.createConfirmation.name} por confirmar
+                          sua presença, nos encontramos no grande dia!
+                        </p>
+                      );
+                    return (
+                      <form
+                        action=""
+                        onSubmit={e => {
+                          e.preventDefault();
+                          createConfirmation({
+                            variables: {
+                              ...this.state,
+                              adults: parseInt(this.state.adults, 10)
+                            }
+                          });
+                        }}
                       >
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                      </select>
-                    </div>
-                    <Button disabled={loading}>
-                      {loading ? "Carregando" : "Confirmar"}
-                    </Button>
-                  </form>
-                );
-              }}
-            </Mutation>
-          </StyledForm>
+                        <div className="form-group">
+                          <input
+                            required
+                            type="text"
+                            placeholder="Nome"
+                            name="name"
+                            value={this.state.name}
+                            onChange={this.handleInput}
+                          />
+                        </div>
+                        <div className="form-group">
+                          Você irá ao evento?{" "}
+                          <input
+                            name="confirmation"
+                            type="checkbox"
+                            id="yes"
+                            checked={
+                              this.state.confirmation === "yes" ? true : false
+                            }
+                            value="yes"
+                            onChange={this.handleInput}
+                          />{" "}
+                          <label htmlFor="yes">Sim</label>
+                          <input
+                            name="confirmation"
+                            type="checkbox"
+                            id="no"
+                            checked={
+                              this.state.confirmation === "no" ? true : false
+                            }
+                            value="no"
+                            onChange={this.handleInput}
+                          />{" "}
+                          <label htmlFor="no">Não</label>
+                        </div>
+                        <div className="form-group">
+                          Quantos adultos?{" "}
+                          <select
+                            required
+                            value={this.state.adults}
+                            name="adults"
+                            onChange={this.handleInput}
+                          >
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                          </select>
+                        </div>
+                        <Button disabled={loading}>
+                          {loading ? "Carregando" : "Confirmar"}
+                        </Button>
+                      </form>
+                    );
+                  }}
+                </Mutation>
+                <h2 className="link">
+                  <Link scroll href="/products" as="/lista-de-presentes">
+                    Veja nossa lista de presentes
+                  </Link>
+                </h2>
+              </StyledForm>
+            </Col>
+          </Row>
         </Col>
       </div>
     );
@@ -189,5 +199,14 @@ const StyledForm = styled.div`
   select {
     padding-top: 0.25rem;
     padding-bottom: 0.25rem;
+  }
+
+  .link {
+    a {
+      color: ${({ theme }) => theme.color.primary};
+    }
+
+    margin-top: 3rem;
+    margin-bottom: 0;
   }
 `;
